ac7a01b5ccdd9dd85d47697e02577b9438575f0c
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-07 15:48:28 -0700

    added second test for stack.pop

--- index.test.js
+++ index.test.js
@@ -1,9 +1,16 @@
 const { Stack } = require("./index.js");
 describe("Stack Class", () => {
-  it("#push should add a new item to the stack ", () => {
+  it.skip("#push should add a new item to the stack ", () => {
     const stack = new Stack();
     stack.push(1);
     console.log(stack.peek);
+    expect(stack.peek()).toEqual(1);
+  });
+  it("#pop should remove the top item of the stack", () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.pop;
     expect(stack.peek()).toEqual(1);
   });
   // add more tests here...
