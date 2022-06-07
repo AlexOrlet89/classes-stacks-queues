94f1283f9158af67fb4b9c0057b67445d4fdcacf
Alex Orlet <94670868+AlexOrlet89@users.noreply.github.com>
2022-06-07 15:22:03 -0700

    pushing to main so we can use VScode

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-class Stack {}
+class Stack {
+    #stack = [];
+
+    constructor(initialStack) {
+        if (initialStack) this.#stack = initialStack;
+    }
+
+    addToTop(item) {
+    this.#stack.push(item);
+    }
+    get takeFromTop() {
+    return this.#stack.pop();
+    }
+    get peekAtTop() {
+    return this.#stack.peek();
+    }
+
+
+}
 
 class Queue {}
 
